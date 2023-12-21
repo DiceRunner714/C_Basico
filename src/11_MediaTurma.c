d5d5208de8db5e332c54d2b59dccb6e8730ad613
DiceRunner714 <eduardo.sandes6@gmail.com>
2023-12-21 19:19:43 -0300

    adiciona file 11_MediaTurma

---
+++ src/11_MediaTurma.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    char q = 'w';
+    float nota;
+    float alunos = 0;
+    float soma_das_notas = 0;
+    float media = 0;
+
+    while(q != 'q')
+    {
+        printf("Digite a proxima Nota\n");
+        scanf("%f", &nota);
+        fflush(stdin);
+
+        printf("Digite 'q' se tiver acabado, caso contrario, digite outra letra\n");
+        scanf("%c", &q);
+        fflush(stdin);
+
+        soma_das_notas += nota;
+        alunos++;
+        printf("\nO numero de alunos eh: %.2f\n", alunos);
+    }
+
+    media = soma_das_notas/alunos;
+
+    printf("\nA media da turma e igual a %.2f\n", media);
+
+    return 0;
+}
